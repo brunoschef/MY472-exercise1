43af68dd7f069c8b17779318adc1c5a5100edb53
brunoschef <wscheffler1104@gmail.com>
2023-10-31 15:26:40 +0000

    created first function in exercise1.r that adds variable 'difference' to dataset

--- Exercise1.R
+++ Exercise1.R
@@ -1,2 +1,11 @@
+approval <- read_csv("bidentrumpapproval.csv")
 
+# Making function that adds new variable 'difference'
+rating_difference <- function(dataset, month_col, president1_col, president2_col) {
+  dataset$approval_difference <- dataset[[president1_col]] - dataset[[president2_col]]
+  return(dataset)
+}
+
+
+approvalwithdiff <- rating_difference(approval, "Month", "BidenAvg", "TrumpAvg")
 
